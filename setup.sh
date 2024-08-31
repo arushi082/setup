1a0e020d49898992459e31686ed9987c3dea67d7
Arushi <59722636+arushi082@users.noreply.github.com>
2024-08-31 18:58:33 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,31 +13,3 @@ python3.11 --version
 
 # Clone the GitHub repository
 echo "Cloning the GitHub repository..."
-git clone https://github.com/arushi082/ChartMetric.git
-
-# Navigate to the cloned repository directory
-cd your-repo-directory
-
-# Provide executable access to this file
-chmod +x setup.sh
-
-# Run this file
-./setup.sh
-
-# Run the actual code
-python3.11 -m src.async_main
-
-# Create a new directory named 'data'
-mkdir data
-
-# Create a virtual environment named 'venv'
-python3.11 -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Upgrade pip to the latest version
-pip install --upgrade pip
-
-# Install the required packages from requirements.txt
-pip install -r requirements.txt
