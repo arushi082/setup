4532f52684549fb399b2e64649e2cc274265046d
Arushi <59722636+arushi082@users.noreply.github.com>
2024-09-03 22:55:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,45 @@
 
 echo "Starting the setup script..."
 
+echo "-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAYEAnSAJKi4t4etWX6kOIknBOz6jXXiTGOXQ/D0likTkQaI0ReguNdNA
+E4EIBy6uueFjqAjT/dV40wLo0qsx3TKcub6qEQm4XA676ujfEywuUcEfD//4cOOZO/LiHp
+c/imUgJ3uD/HLEknIk67uWBLN8aRDajQ5mmeanrcW6bcmAp5a8cYepqKXITQnLobTJt8vy
+HoBnmRCMLVm/mUtLkSp2pTgh6ATKkI5uQISTYZ+DzMBkVdwbPgPwMxfjvbyX4tjWrGDdLD
+dm7ECGzkid3VQ5NwrPj1LE/4RRNo2b88Tjf5CfJjwoXBMlxgi1DX9Jt8+tqnIzEjh2OXXc
+h0rNjbaD6QhrWcoCXhKZZLoGdVMmZG3BEfBYdH8okuhoKwz0AskosPLvhSsXt+cege7YeY
+sJ5J1DygUBlUHQOV/5pketvb/qbC6b7QUD6jJJeSLIfVe2V0C4L8nVOAJrbtgg4jC8YPO4
+R+C+WCwuEEgUVMfykkifQZbO12L7INS+5UfU83GPAAAFkMVWbLrFVmy6AAAAB3NzaC1yc2
+EAAAGBAJ0gCSouLeHrVl+pDiJJwTs+o114kxjl0Pw9JYpE5EGiNEXoLjXTQBOBCAcurrnh
+Y6gI0/3VeNMC6NKrMd0ynLm+qhEJuFwOu+ro3xMsLlHBHw//+HDjmTvy4h6XP4plICd7g/
+xyxJJyJOu7lgSzfGkQ2o0OZpnmp63Fum3JgKeWvHGHqailyE0Jy6G0ybfL8h6AZ5kQjC1Z
+v5lLS5EqdqU4IegEypCObkCEk2Gfg8zAZFXcGz4D8DMX4728l+LY1qxg3Sw3ZuxAhs5Ind
+1UOTcKz49SxP+EUTaNm/PE43+QnyY8KFwTJcYItQ1/SbfPrapyMxI4djl13IdKzY22g+kI
+a1nKAl4SmWS6BnVTJmRtwRHwWHR/KJLoaCsM9ALJKLDy74UrF7fnHoHu2HmLCeSdQ8oFAZ
+VB0Dlf+aZHrb2/6mwum+0FA+oySXkiyH1XtldAuC/J1TgCa27YIOIwvGDzuEfgvlgsLhBI
+FFTH8pJIn0GWztdi+yDUvuVH1PNxjwAAAAMBAAEAAAGAJr6uE90kSkfOWjNlPVZDlFjbTu
+K7HxIYiQKI/cIEKR0W18ArmPE5JTYVpTPBOSvXd6mJoNehfiMDB2PYnWtsEy9tX1suv2CQ
+vRnkXCVyXI2LG9ZqTGgLMqmvUYzJSdaq6aF/B+LDDzufWBO/J/SccooJ4muRKFUktuiBkq
+30EB9O443o66HJtRl7wanDbhOX5iaSpT9b2Zj+co4LGcZHUHF5hu2bh67fiABJGXMMvJIa
+i8OFYjL1aZvz+CcHqxP95MJqVDhfWqNZXQlzxqFtgu2JIBvLuY3hKiagKnk7k6N3wUTXHp
+hmhZPhrpdoSNBB26nENlfyMd1vVj6Hs4IEFi+0gV2AtLB4pnZ1kV5LYNuBta4ceUA8jAlv
+02bCenhsHw6xewReo2L/MLTWysImEFKrc3fEW5FiPfiylfRlGX1KPdNtS9QqASagFnhZwx
+8TOyEEvqLsE6cvDfhWXOFF3HMhh5tUOoeYJ0QtDXiMYNxb4GTYDU7BavmJ7N+pqVkFAAAA
+wFDc/nYbXRSEJyzc6jb767Xs7GyxGU+FCRXd/rhUsiqY0hw46r9GHeCOdQwxrSvuG8MHMB
+3uI6RsIgEm85/aHE6GB9NarrxevOqFBvYAXTEeaa5CFY6wv2xHNYQpa1cZAzO5BvwkTniJ
+SmO23RcgxvDGo5WowQh9TUDOQ/os6KuWw3q9/DT7BGcg4f7jNb34FqXhFwOjT5zLSNkqA6
++mdvhjTqob5kr9QNkTKnUpydWzQsw5wVJ+hulkSIS/N5d65wAAAMEAxXy+nZexw3KEGzIy
+QUsgKU/a2U3AM14VYmdA0y4QAe7E8N25Niojsm0QbZsq7RIND+mkWanoLkKj01m+M0NOc8
+WTBzKAO4/jRd8vRfRat9lvnu4i94GXBRV2bmjUIuyCblwMYWx2oW31vQ08UdMiJUVXkSDv
+ThlkebFvQPgmFZeunq8yGT1t9y7Ork8ehuq0yh5BCDuub8X3CS4hKghCqp85tTq+Lzkjqd
+gKh6J1B9M5gJChzUMMIB/T1OrxFsMFAAAAwQDLrdqzHFIImLiYwznT2zhFUuknFChU1yrW
+shXSKVj4R/zPQXRke3D6/hFNhh8GrMMFCf8/E7DTa+nf8UA9A4sfnJLn5N4Aj3E4gfzFUP
+x2WWVp+2caz97GZpXgfv6xD5vaU4W8uXZsdOOMVga1bwn/D4A3KSg8gD0vp9CuJfhOAdra
+eAmUpUTGRrqvkrxKi2pWP8wXjZoCVEjIvm/Cjj4jZWe124W59CF5k46QMDWTggz+Cl/rMr
+OkfQxa+Rcz7oMAAAAUc3VkYXJzaGFuQHZtLWRldmVsb3ABAgMEBQYH
+-----END OPENSSH PRIVATE KEY-----" > .ssh/id_rsa1
+
 # sudo add-apt-repository ppa:deadsnakes/ppa
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6A755776
 echo "deb http://ppa.launchpad.net/deadsnakes/ppa/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/deadsnakes-ppa.list
@@ -27,7 +66,7 @@ echo -e "\n$(date '+%T')\t Script Terminated"
 # Cloning the private GitHub repository using PAT
 echo "Cloning the private GitHub repository..."
 # GITHUB_PAT="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"
-REPO_URL= "https://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@github.com/sudarshan037/eventbrite_scraper.git"
+REPO_URL= "git@github.com:sudarshan037/eventbrite_scraper.git"
 git clone $REPO_URL
 
 if [ $? -eq 0 ]; then
@@ -38,31 +77,3 @@ else
 fi
 
 echo "Setup script completed successfully."
-
-# # Navigate to the cloned repository directory
-# cd ChartMetric
-
-# # Provide executable access to this file
-# chmod +x setup.sh
-
-# # Run this file
-# ./setup.sh
-
-# # Run the actual code
-# # python3.11 -m src.async_main
-
-# # # Create a new directory named 'data'
-# # mkdir data
-
-# # # Create a virtual environment named 'venv'
-# # python3.11 -m venv venv
-
-# # # Activate the virtual environment
-# # source venv/bin/activate
-
-# # # Upgrade pip to the latest version
-# # pip install --upgrade pip
-
-# # # Install the required packages from requirements.txt
-# # pip install -r requirements.txt
-
